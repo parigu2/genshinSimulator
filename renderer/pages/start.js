f43b0f8947911965743057e1cde169f21bee5a1b
minkyu <parigu2@msn.com>
2020-12-04 23:12:24 -0600

    chore: test updating app works

--- renderer/pages/start.js
+++ renderer/pages/start.js
@@ -2,7 +2,7 @@ import FirstPage from './firstPage'
 
 export default () => (
   <div>
-    <span>This is Next.js speaking - first commit</span>
+    <span>This is Next.js speaking - first commit (updated)</span>
     <FirstPage />
   </div>
 )
