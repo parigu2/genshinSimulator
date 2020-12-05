22a5d2e28e7ca8d27b7c0119d7d13aa71d3ade6f
minkyu <parigu2@msn.com>
2020-12-04 23:22:03 -0600

    chore: update 2nd

--- renderer/pages/start.js
+++ renderer/pages/start.js
@@ -2,7 +2,7 @@ import FirstPage from './firstPage'
 
 export default () => (
   <div>
-    <span>This is Next.js speaking - first commit (updated)</span>
+    <span>This is Next.js speaking - first commit (updated - 2)</span>
     <FirstPage />
   </div>
 )
